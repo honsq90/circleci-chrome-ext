279eda1b01a88979d83ccfdfb704eaa095356e1d
Shuqian Hon (SQ) <honsq90@gmail.com>
2018-10-15 11:48:12 +1100

    feat: add lastUpdated property and background task

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,10 @@
 
 function polling() {
+  chrome.storage.sync.get({
+    builds: [],
+  }, ({ builds }) => {
+    console.log('polling', builds);
+  });
   setTimeout(polling, 1000 * 30);
 }
 
--- src/popup.ts
+++ src/popup.ts
@@ -1,21 +1,37 @@
 import * as $ from 'jquery';
-import { extractProjectData, Build } from './xmlExtract';
+import { forkJoin, interval, of } from 'rxjs';
+import { mergeMap, switchMap, tap, map } from 'rxjs/operators';
+import { Build, extractProjectData } from './xmlExtract';
 
+const convertToPromise = (val: Build, index: number) => {
+  return $.get(val.xmlUrl)
+    .then(extractProjectData)
+    .then((build) => ({ ...build, xmlUrl: val.xmlUrl }));
+}
 
 $(function () {
 
   chrome.storage.sync.get({
     builds: [],
-  }, ({ builds }) => {
-    builds.forEach((build: Build, index) => {
-
-      $.get(build.xmlUrl)
-        .then(extractProjectData)
-        .then(({ lastBuildStatus, activity }: Build) => {
-          $(`#build_${index}`).addClass(lastBuildStatus);
-          $(`#build_${index}`).addClass(activity);
+    lastUpdated: null,
+  }, ({ builds, lastUpdated }) => {
+    interval(5000)
+      .pipe(
+        switchMap(() => of(builds).pipe(
+          mergeMap((build) => forkJoin(...build.map(convertToPromise))),
+          map((builds) => ({ builds, lastUpdated: new Date() })),
+          tap((syncState) => chrome.storage.sync.set(syncState)),
+        )),
+      )
+      .subscribe(({ builds, lastUpdated }) => {
+        builds.forEach((build: Build, index) => {
+          $(`#build_${index}`).addClass(build.lastBuildStatus);
+          $(`#build_${index}`).addClass(build.activity);
         });
+        $(`#update_status`).text(`Last updated at ${lastUpdated.toISOString()}`);
+      });
 
+    builds.forEach((build: Build, index) => {
       const buildDivAttributes = {
         id: `build_${index}`,
         class: build.lastBuildStatus,
@@ -29,6 +45,10 @@ $(function () {
             target: '_blank',
           }))
         );
+
+      if (lastUpdated) {
+        $(`#update_status`).text(`Last updated at ${lastUpdated.toISOString()}`);
+      }
 
     });
   });
